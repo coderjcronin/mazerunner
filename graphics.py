fbfa3fb45c0d05551ea10fbd200d5ab7568cfb71
coderjcronin <coderjmc@gmail.com>
2025-01-31 20:29:51 -0500

    Initial testing window with TKinter

---
+++ graphics.py
@@ -0,0 +1,23 @@
+from tkinter import Tk, BOTH, Canvas
+
+class Window:
+    def __init__(self, height, width):
+        self.__root = Tk()
+        self.__root.title = "Testing"
+        self.__root.protocol("WM_DELETE_WINDOW", self.close)
+        self.__canvas = Canvas(self.__root, bg="white", height=height, width=width)
+        self.__canvas.pack(fill=BOTH, expand=1)
+        self.running = False
+
+    def redraw(self):
+        self.__root.update_idletasks()
+        self.__root.update()
+    
+    def wait_for_close(self):
+        self.running = True
+        while self.running:
+            self.redraw()
+        print("Window closed.")
+
+    def close(self):
+        self.running = False
